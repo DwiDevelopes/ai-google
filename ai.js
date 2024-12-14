99e47c69d37285274ccf626048d24e805083fbb1
Dwi Bakti N Dev / Roy <166002502+DwiDevelopes@users.noreply.github.com>
2024-12-14 16:47:06 +0700

    ai.js

--- ai.js
+++ ai.js
@@ -10,7 +10,7 @@ let isResponseGenerating = false;
 
 // API configuration
 const API_KEY = "AIzaSyCMeo9xDnRApzxP_PQYZM7Jd4gcKHnAQ64"; // Your API key here
-const API_URL = "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=YOUR_API_KEY";
+const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=YOUR_API_KEY`;
 
 // Load theme and chat data from local storage on page load
 const loadDataFromLocalstorage = () => {
